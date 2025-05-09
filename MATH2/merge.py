57824e6acc69df0641e782be405db3b2c8c103d0
Panda-Ghost <billy5902@outlook.com>
2025-05-09 21:29:14 +0800

    autoupd

---
+++ MATH2/merge.py
@@ -0,0 +1,15 @@
+import os
+os.system('cd /Users/wangboxiang/Documents/Notes/MATH2')
+os.system('pwd')
+
+ofile=open('math.md', 'w')
+
+for i in range(1, 50):
+    filename=f'./MATH-{str(i)}/MATH-{str(i)}.md'
+    if os.path.exists(filename):
+        with open(filename, 'r') as ifile:
+            lines=ifile.readlines()
+            for j in range(2): lines.append('\n')
+            for line in lines:
+                ofile.write(line)
+    
