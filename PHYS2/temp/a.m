df95cd9375b9890a18a899e0d1fc60c90161b293
Panda-Ghost <billy5902@outlook.com>
2025-05-29 14:36:30 +0800

    autoupd

--- PHYS2/temp/a.m
+++ PHYS2/temp/a.m
@@ -77,4 +77,6 @@ set(gca, 'FontName', 'Latin Modern Math', 'FontSize', 15);
 
 subplot(1, 2, 2);
 plot(v1, eta, '.-k', 'LineWidth', 2, 'MarkerSize', 15);
+xlabel('\fontname{Songti SC}风速 \fontname{Latin Modern Math}V_1/(m/s)');
+ylabel('\fontname{Songti SC}发电机转换效率 \fontname{Latin Modern Math}\eta ');
 set(gca, 'FontName', 'Latin Modern Math', 'FontSize', 15);
