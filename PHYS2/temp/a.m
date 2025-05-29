ce087e86b2d1aeae54b67a6360b78be7d85e38b9
Panda-Ghost <billy5902@outlook.com>
2025-05-29 14:08:43 +0800

    autoupd

--- PHYS2/temp/a.m
+++ PHYS2/temp/a.m
@@ -11,7 +11,7 @@ ita=p1./p0
 subplot(1, 2, 1);
 uv=scatter(u, v1, 200, 'k', '.');
 dddd=(0:0.25:2)-0.3;
-text(u-dddd, v1+0.15, arrayfun(@(x)sprintf("%.3f", x), v1), 'fontname', 'latin modern math', 'fontsize', 12)
+text(u-dddd, v1+0.12, arrayfun(@(x)sprintf("%.3f", x), v1), 'fontname', 'latin modern math', 'fontsize', 12)
 
 hold on;
 xlim([10, 26])
