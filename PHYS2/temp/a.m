5ba4814dbe1de83a818b9ca50c689d5d89ac256e
Panda-Ghost <billy5902@outlook.com>
2025-05-29 13:47:49 +0800

    autoupd

--- PHYS2/temp/a.m
+++ PHYS2/temp/a.m
@@ -44,6 +44,7 @@ set(gca, 'FontName', 'Latin Modern Math', 'FontSize', 15);
 xlim([400, 2000])
 xticks(r)
 
-legend(["\fontname{Songti SC}风机电压 \fontname{Latin Modern Math}16V; \fontname{Songti SC}风速 \fontname{Latin Modern Math}4.648m/s", "\fontname{Songti SC}风机电压 \fontname{Latin Modern Math}24V; \fontname{Songti SC}风速 \fontname{Latin Modern Math}7.180m/s"]);
+legend(["\fontname{Songti SC}风机电压 \fontname{Latin Modern Math}16V; \fontname{Songti SC}风速 \fontname{Latin Modern Math}4.648m/s", "\fontname{Songti SC}风机电压 \fontname{Latin Modern Math}24V; \fontname{Songti SC}风速 \fontname{Latin Modern Math}7.180m/s"],...
+    'FontSize', 15);
 xlabel('\fontname{Songti SC}负载电阻 \fontname{Latin Modern Math}R/\Omega');
 ylabel('\fontname{Songti SC}输出功率 \fontname{Latin Modern Math}P/W');
