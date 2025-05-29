928ccc39c56f98ebeaf0f5bd4c9ce3925276968d
Panda-Ghost <billy5902@outlook.com>
2025-05-29 14:50:07 +0800

    autoupd

--- PHYS2/temp/a.m
+++ PHYS2/temp/a.m
@@ -69,8 +69,8 @@ rho=1.185;
 p1=0.5.*rho.*s.*(v1.^3);
 eta=p0./p1;
 
-fprintf("%d | ", u);
-fprintf("\n");
+fprintf("| %d ", u);
+fprintf("| \n");
 
 % subplot(1, 2, 1);
 % plot(v1, p0, '.-k', 'LineWidth', 2, 'MarkerSize', 15);
