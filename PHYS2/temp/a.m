59eb558ed55aa10fb2a851b6e3e46203f333a697
Panda-Ghost <billy5902@outlook.com>
2025-05-29 13:55:48 +0800

    autoupd

--- PHYS2/temp/a.m
+++ PHYS2/temp/a.m
@@ -49,3 +49,4 @@ legend(["\fontname{Songti SC}风机电压 \fontname{Latin Modern Math}16V; \font
 xlabel('\fontname{Songti SC}负载电阻 \fontname{Latin Modern Math}R/\Omega');
 ylabel('\fontname{Songti SC}输出功率 \fontname{Latin Modern Math}P/W');
 text(r, p1+0.001, arrayfun(@(x)sprintf("%.4f", x), p1), 'fontname', 'latin modern math')
+text(r, p2+0.001, arrayfun(@(x)sprintf("%.4f", x), p2), 'fontname', 'latin modern math')
